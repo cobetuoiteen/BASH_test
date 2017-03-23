2a7ecc24b754095e2b7122f682082e916cfa047c
cobetuoiteen <tienhuynhq5@gmail.com>
2017-03-23 16:57:06 +0700

    Thứ năm, 23 Tháng ba năm 2017 16:57:06 ICT : code nay duoc push tu dong boi script

--- exercises/exercise_1.sh
+++ exercises/exercise_1.sh
@@ -2,5 +2,31 @@
 
 option_handle()
 {
-	
+	case "$1" in
+		"-h" | "--help" ) #print usage ;;
+		"-d" ) #print current dir;;
+		"--cd" "dir" ) #cd vao dir;;
+		"-n" ) #show number agrumnent;;
+		"-s" 'string' ) #printf **string**;;
+		* ) echo "error $1"
+	esac
+exit 0	
 }
+
+queue_option()
+{
+	for option in $*
+	do
+		option_handle $option
+	done
+exit 0
+}
+
+echo 
+echo
+echo
+echo
+echo
+echo
+
+queue_option $*
